6ad47a5636ec040f24476241f4b3ed30d04f16bd
jacekgry <jacek.grycza@student.put.poznan.pl>
2020-05-16 01:10:03 +0200

    initial commit

--- eth-voting/README.md
+++ eth-voting/README.md
@@ -1,6 +1,6 @@
 # EthVoting
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.1.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.6.
 
 ## Development server
 
--- eth-voting/src/app/app.component.ts
+++ eth-voting/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'eth-voting';
+
+  votingSearchForm = this.fb.group({
+    votingId: ['']
+  });
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  searchForVoting(event) {
+    console.log(this.votingSearchForm.value.votingId);
+  }
 }
