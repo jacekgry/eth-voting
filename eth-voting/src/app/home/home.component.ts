1dd6264a5b5a6b6ca5b6cc3c6b478e16866bb636
unknown <artur.mierzwa97@gmail.com>
2020-09-07 19:58:30 +0200

    added voting results

--- eth-voting/src/app/home/home.component.ts
+++ eth-voting/src/app/home/home.component.ts
@@ -9,6 +9,13 @@ import {VotingService} from '../services/voting.service';
 })
 export class HomeComponent implements OnInit {
 
+  options = ["1sfaaaaaaasfasfasfasfasfascascasdasf", "2", "3"];
+  votes = ["0", "1", "0"];
+  percentage = [0, 100, 0]
+  numberOfVotes = 0;
+  numberOfAllVoters = 10;
+  isEnded = false;
+
   votingStartForm = this.fb.group({
     votingId: ['']
   });
@@ -61,10 +68,24 @@ export class HomeComponent implements OnInit {
     const votingId = this.votingResultForm.value.votingId;
     console.log('voting id: ', votingId);
     this.votingService.getAccount().then(a => console.log('Account: ', a));
-
+    this.percentage.splice(0);
     this.votingService.getVotes(votingId)
+      .then(votes => {
+        console.log('Result: ', votes);
+        this.votes = votes.map(Number);
+        this.numberOfVotes = votes.reduce((a, b) => a + b, 0)
+        this.votes.forEach(vote => {
+          this.percentage.push((vote as unknown as number) / this.numberOfVotes  * 100)
+        })
+      })
+      .catch(error => {
+        console.log('Result: ', error);
+      });
+
+    this.votingService.getVoting(votingId)
       .then(options => {
         console.log('Result: ', options);
+        this.options = options.slice(1);
       })
       .catch(error => {
         console.log('Result: ', error);
